ea1b470cbf054d2c506d5b88f7cf2b8d3fb93b33
renan_rinaldi <renan@rcrconsult.com.br>
2023-09-03 20:41:06 -0300

    feat: implementando o método para atualizar o status do pedido.

--- src/src/Adapters/Driving/Api/Controllers/PedidoController.cs
+++ src/src/Adapters/Driving/Api/Controllers/PedidoController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using TechChallenge.src.Adapters.Driving.Api.Controllers.Base;
 using TechChallenge.src.Core.Domain.Adapters;
 using TechChallenge.src.Core.Domain.Commands.Pedidos;
+using TechChallenge.src.Core.Domain.Enums;
 
 namespace TechChallenge.src.Adapters.Driving.Api.Controllers
 {
@@ -50,6 +51,22 @@ namespace TechChallenge.src.Adapters.Driving.Api.Controllers
             if (!IsOperacaoValida) return BadRequest(ObterNotificacoes());
 
             return Ok(entidade);
+        }
+
+        [HttpPost("AtualizaStatusPedido")]
+        public async Task<IActionResult?> PostStatusPedido(string numeroDoPedido, int idStatusPedido)
+        {
+            if (!ModelState.IsValid) return null;
+
+            var pedido = (await _pedidoRepository.Buscar(x => x.NumeroPedido == numeroDoPedido)).FirstOrDefault();
+
+            if (pedido == null) return BadRequest("Pedido não enconntrado.");
+
+            pedido.StatusPedido = (EStatusPedido)idStatusPedido;
+
+            var status = _pedidoRepository.Atualizar(pedido);
+
+            return Ok(pedido);
         }
     }
 }
